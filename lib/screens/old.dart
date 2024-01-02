9d39664e2b7ca918496994e3670ae60e71854ec5
jcbecleo <ecleojosscharyborj@gmail.com>
2024-01-02 23:39:18 +0800

    Reworked Profile Screen

--- lib/screens/old.dart
+++ lib/screens/old.dart
@@ -1,243 +1,156 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_weather/models/StorageItem.dart';
+import 'package:weather_weather/screens/login_screen.dart';
+import 'package:weather_weather/screens/dashboard_screen.dart';
+import 'package:weather_weather/screens/forecast_screen.dart';
+import 'package:weather_weather/services/StorageServices.dart';
+import 'package:weather_weather/widgets/InfoField.dart';
+import 'package:weather_weather/widgets/BottomNavbar.dart'; // Import the BottomNavbar widget
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(0xFF011627),
-//       extendBodyBehindAppBar: true,
-//       appBar: AppBar(
-//         backgroundColor: Colors.transparent,
-//         elevation: 0,
-//         systemOverlayStyle: SystemUiOverlayStyle(
-//           statusBarBrightness: Brightness.dark,
-//         ),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.fromLTRB(45, 1 * kToolbarHeight, 45, 20),
-//         child: SizedBox(
-//           height: MediaQuery.of(context).size.height,
-//           child: Stack(
-//             children: [
-//               Align(
-//                 alignment: AlignmentDirectional(5, -0.3),
-//                 child: Container(
-//                   height: 300,
-//                   width: 300,
-//                   decoration: BoxDecoration(
-//                       shape: BoxShape.circle, color: Colors.white),
-//                 ),
-//               ),
-//               Align(
-//                 alignment: AlignmentDirectional(0, -1.2),
-//                 child: Container(
-//                   height: 300,
-//                   width: 600,
-//                   decoration: BoxDecoration(color: Colors.orange),
-//                 ),
-//               ),
-//               BackdropFilter(
-//                 filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
-//                 child: Container(
-//                   decoration: BoxDecoration(
-//                     color: Colors.transparent,
-//                   ),
-//                 ),
-//               ),
-//               BlocBuilder<WeatherBlocBloc, WeatherBlocState>(
-//                 builder: (context, state) {
-//                   return SizedBox(
-//                     width: MediaQuery.of(context).size.width,
-//                     height: MediaQuery.of(context).size.height,
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           '📍 Cebu City',
-//                           style: TextStyle(
-//                               color: Colors.white, fontWeight: FontWeight.w300),
-//                         ),
-//                         Text(
-//                           'Good morning!',
-//                           style: TextStyle(
-//                               color: Colors.white,
-//                               fontSize: 25,
-//                               fontWeight: FontWeight.bold),
-//                         ),
-//                         Image.asset(
-//                           'assets/1.png',
-//                           scale: 1,
-//                         ),
-//                         Center(
-//                           child: Text(
-//                             '21°C',
-//                             style: TextStyle(
-//                                 color: Colors.white,
-//                                 fontSize: 53,
-//                                 fontWeight: FontWeight.w600),
-//                           ),
-//                         ),
-//                         Center(
-//                           child: Text('Thunderstorm',
-//                               style: TextStyle(
-//                                   color: Colors.white,
-//                                   fontSize: 20,
-//                                   fontWeight: FontWeight.w300)),
-//                         ),
-//                         Center(
-//                             child: Text('Tuesday 2 · 12:00AM',
-//                                 style: TextStyle(
-//                                     color: Colors.white, fontSize: 13))),
-//                         SizedBox(height: 25),
-//                         Row(
-//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                           children: [
-//                             Row(
-//                               children: [
-//                                 Image.asset(
-//                                   'assets/11.png',
-//                                   scale: 8,
-//                                 ),
-//                                 Column(
-//                                   crossAxisAlignment: CrossAxisAlignment.start,
-//                                   children: [
-//                                     Text(
-//                                       'Sunrise',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w300,
-//                                       ),
-//                                     ),
-//                                     SizedBox(height: 3),
-//                                     Text(
-//                                       '5:00AM',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w700,
-//                                       ),
-//                                     ),
-//                                   ],
-//                                 ),
-//                               ],
-//                             ),
-//                             Row(
-//                               children: [
-//                                 Image.asset(
-//                                   'assets/12.png',
-//                                   scale: 8,
-//                                 ),
-//                                 Column(
-//                                   crossAxisAlignment: CrossAxisAlignment.start,
-//                                   children: [
-//                                     Text(
-//                                       'Sunset',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w300,
-//                                       ),
-//                                     ),
-//                                     SizedBox(height: 3),
-//                                     Text(
-//                                       '5:30PM',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w700,
-//                                       ),
-//                                     ),
-//                                   ],
-//                                 ),
-//                               ],
-//                             ),
-//                           ],
-//                         ),
-//                         Padding(
-//                           padding: EdgeInsets.symmetric(vertical: 5.0),
-//                           child: Divider(
-//                             color: Colors.blueGrey,
-//                           ),
-//                         ),
-//                         Row(
-//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                           children: [
-//                             Row(
-//                               children: [
-//                                 Image.asset(
-//                                   'assets/13.png',
-//                                   scale: 8,
-//                                 ),
-//                                 Column(
-//                                   crossAxisAlignment: CrossAxisAlignment.start,
-//                                   children: [
-//                                     Text(
-//                                       'Temp Max',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w300,
-//                                       ),
-//                                     ),
-//                                     SizedBox(height: 3),
-//                                     Text(
-//                                       '25°C',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w700,
-//                                       ),
-//                                     ),
-//                                   ],
-//                                 ),
-//                               ],
-//                             ),
-//                             Row(
-//                               children: [
-//                                 Image.asset(
-//                                   'assets/14.png',
-//                                   scale: 8,
-//                                 ),
-//                                 Column(
-//                                   crossAxisAlignment: CrossAxisAlignment.start,
-//                                   children: [
-//                                     Text(
-//                                       'Temp Min',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w300,
-//                                       ),
-//                                     ),
-//                                     SizedBox(height: 3),
-//                                     Text(
-//                                       '21°C',
-//                                       style: TextStyle(
-//                                         color: Colors.white,
-//                                         fontWeight: FontWeight.w700,
-//                                       ),
-//                                     ),
-//                                   ],
-//                                 ),
-//                               ],
-//                             ),
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                   );
-//                 },
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//       bottomNavigationBar: BottomNavbar(
-//         currentIndex: _currentIndex,
-//         onTap: (index) {
-//           setState(() {
-//             _currentIndex = index;
-//           });
-//           // Handle navigation or any other logic based on the selected index
-//           if (_currentIndex == 1) {
-//             Navigator.pushNamed(context, ForecastScreen.routeName);
-//           } else if (_currentIndex == 2) {
-//             Navigator.pushNamed(context, ProfileScreen.routeName);
-//           }
-//         },
-//       ),
-//     );
-//   }
-// }
+class ProfileScreen extends StatelessWidget {
+  static String routeName = "/profile";
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final StorageService storageService = StorageService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/bg_1.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(80.0),
+              child: Text(
+                'User Profile',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey, // Set the shadow color
+                      offset: Offset(2.0, 2.0), // Set the shadow offset
+                      blurRadius: 4.0, // Set the blur radius
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 50.0),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'Welcome to your profile screen!',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                FutureBuilder(
+                  future: getUserDetails(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      User? user = snapshot.data as User?;
+                      if (user == null) {
+                        return Text('No user logged in.');
+                      } else {
+                        String displayName =
+                            user.displayName ?? user.email!.split('@')[0];
+                        String email = user.email ?? "";
+
+                        return Column(
+                          children: [
+                            // Circular icon (using Photo URL or default person icon)
+                            CircleAvatar(
+                              radius: 50.0,
+                              backgroundImage: user.photoURL != null
+                                  ? NetworkImage(user.photoURL!)
+                                  : AssetImage('assets/default_pfp.jpg')
+                                      as ImageProvider<Object>?,
+                            ),
+                            // Display Name
+                            InfoField(
+                                labelText: 'Full Name', content: displayName),
+                            // Email Address
+                            InfoField(
+                                labelText: 'Email Address', content: email),
+                            SizedBox(height: 20.0),
+                            ElevatedButton(
+                              onPressed: () {
+                                // Handle logout button press
+                                logoutUser(context);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                foregroundColor: Colors.blue,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      20.0), // Set the border radius
+                                ),
+                                minimumSize: Size(150.0, 40),
+                              ),
+                              child: Text('Log Out'),
+                            ),
+                          ],
+                        );
+                      }
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavbar(
+        currentIndex: 2, // Set the appropriate index for the Profile screen
+        onTap: (index) {
+          // Handle navigation or any other logic based on the selected index
+          // You can add more cases for other screen navigation if needed
+          if (index == 0) {
+            Navigator.pushNamed(context, DashboardScreen.routeName);
+          } else if (index == 1) {
+            Navigator.pushNamed(context, ForecastScreen.routeName);
+          }
+        },
+      ),
+    );
+  }
+
+  Future<User?> getUserDetails() async {
+    User? user = _auth.currentUser;
+    return user;
+  }
+
+  void logoutUser(BuildContext context) async {
+    try {
+      await _auth.signOut();
+      await storageService.saveData(StorageItem("uid", null));
+      Navigator.pushReplacementNamed(context, LoginScreen.routeName);
+    } catch (e) {
+      print("Error logging out: $e");
+    }
+  }
+}
